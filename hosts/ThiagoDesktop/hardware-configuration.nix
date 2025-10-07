94f03ee5c08b2e68463119f67b25b0eb3a4805d1
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2025-10-06 21:33:52 -0300

    forgot the disk-config import

--- hosts/ThiagoDesktop/hardware-configuration.nix
+++ hosts/ThiagoDesktop/hardware-configuration.nix
@@ -2,7 +2,9 @@
 # and may be overwritten by future invocations.  Please make changes
 # to /etc/nixos/configuration.nix instead.
 { lib, pkgs, modulesPath, ... }: {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  imports = [ 
+ ./disk-config.nix
+  (modulesPath + "/installer/scan/not-detected.nix") ];
 
   boot.initrd.availableKernelModules =
     [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
@@ -12,13 +14,7 @@
 
   boot.kernelPackages = pkgs.linuxPackages_lqx;
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp8s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp7s0.useDHCP = lib.mkDefault true;
 
   services.fwupd.enable = true;
   hardware = {
